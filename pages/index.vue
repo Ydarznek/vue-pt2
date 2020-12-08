4662b52cf1f310a37f47ffc216743bba9b099f65
Iuliia Darznek <Iuliia_Darznek@epam.com>
2020-12-08 12:36:37 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">Nuxt</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  color: #35495e;
+  font-family: Roboto, Arial, sans-serif;
+  font-size: 100px;
+  font-weight: 300;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  padding-bottom: 15px;
+  color: #526488;
+  font-size: 42px;
+  font-weight: 300;
+  word-spacing: 5px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
